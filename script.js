c6cd8c5d60daf514b15a64ff094e4f2c776cddd6
Saymon Medeiros <saymon2000@hotmail.com>
2023-11-30 19:51:09 -0300

    :tada:

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleMode() {
+  const html = document.documentElement
+
+ // if(html.classList.contains('light')) {      /*condicional sim ou não*/
+ // html.classList.remove('light')}
+ // else {                                     /* else= senão*/ 
+ // html.classList.add('light')                      
+ // }
+  
+
+   html.classList.toggle('light') /* toggle= se tiver o elemento ele tira e não tiver ele coloca */
+
+}
